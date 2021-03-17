6fcb58e358c6ad981c245ee449505b085d6b1739
Morgan Grovenburg <morgan.grovenburg@gmail.com>
2021-03-16 21:55:04 -0500

    Most Flipbookr Features Template

---
+++ setup.R
@@ -0,0 +1,14 @@
+# Installation
+# Must install to see templates when creating a new RMarkdown
+
+# install devtools to install from github
+install.packages("devtools")
+
+# install the development version of flipbookr with devtools
+devtools::install_github("EvaMaeRey/flipbookr")
+
+# alternatively, install flipbookr with remotes
+remotes::install_github("EvaMaeRey/flipbookr")
+
+# Use this package with the rmarkdown presentation tool, xaringan
+install.packages("xaringan")
